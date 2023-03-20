e86186ac4945ef27d03aa40191f5912c5524c62c
Hehongyuanlove <1016823618@qq.com>
2023-03-20 14:03:12 +0800

    line 注释

--- line.go
+++ line.go
@@ -1,25 +1,30 @@
 package fauxgl
 
+// Line 结构体表示一条线段
 type Line struct {
 	V1, V2 Vertex
 }
 
+// NewLine 用于创建一条线段
 func NewLine(v1, v2 Vertex) *Line {
 	return &Line{v1, v2}
 }
 
+// NewLineForPoints 用于创建一条线段
 func NewLineForPoints(p1, p2 Vector) *Line {
 	v1 := Vertex{Position: p1}
 	v2 := Vertex{Position: p2}
 	return NewLine(v1, v2)
 }
 
+// BoundingBox 用于获取线段的包围盒
 func (l *Line) BoundingBox() Box {
 	min := l.V1.Position.Min(l.V2.Position)
 	max := l.V1.Position.Max(l.V2.Position)
 	return Box{min, max}
 }
 
+// Transform 用于对线段进行变换
 func (l *Line) Transform(matrix Matrix) {
 	l.V1.Position = matrix.MulPosition(l.V1.Position)
 	l.V2.Position = matrix.MulPosition(l.V2.Position)
